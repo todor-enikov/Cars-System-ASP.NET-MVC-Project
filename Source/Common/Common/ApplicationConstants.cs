10da6f4150c946b85ca28cfb22ce93ceed82a69b
todor-enikov <enigmaenikov@gmail.com>
2017-03-12 15:39:44 +0200

    Inserted Common class library.

---
+++ Source/Common/Common/ApplicationConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class ApplicationConstants
+    {
+        public const string AdminRole = "Admin";
+        public const string UserRole = "User";
+    }
+}
