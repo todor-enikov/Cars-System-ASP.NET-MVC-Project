0be0a1c7a94257d3311d28fd986acb043203d942
todor-enikov <enigmaenikov@gmail.com>
2017-03-12 23:03:19 +0200

    Added Arrange, Act & Assert phases into CarsSystemDbContextTests class.

--- Source/Tests/CarsSystem.Tests/Data/CarsSystem.Data/CarsSystemDbContextTests.cs
+++ Source/Tests/CarsSystem.Tests/Data/CarsSystem.Data/CarsSystemDbContextTests.cs
@@ -9,32 +9,40 @@ namespace CarsSystem.Tests.Data.CarsSystem.Data
         [Test]
         public void CarsSystemDbContext_ShouldCreateInstanceOfDatabase()
         {
+            // Arrange
             var db = new CarsSystemDbContext();
 
+            // Act & Assert
             Assert.IsInstanceOf<CarsSystemDbContext>(db);
         }
 
         [Test]
         public void CarsSystemDbContext_ShouldBeTypeOfICarsDbContext()
         {
+            // Arrange
             var db = new CarsSystemDbContext();
 
+            // Act & Assert
             Assert.IsInstanceOf<ICarsSystemDbContext>(db);
         }
 
         [Test]
         public void CarsSystemDbContext_CreateMethodShouldReturnInstanceOfCarsDbContext()
         {
+            // Arrange
             var db = CarsSystemDbContext.Create();
 
+            // Act & Assert
             Assert.IsInstanceOf<CarsSystemDbContext>(db);
         }
 
         [Test]
         public void CarsSystemDbContext_CreateMethodShouldReturnInstanceOfICarsDbContext()
         {
+            // Arrange
             var db = CarsSystemDbContext.Create();
 
+            // Act & Assert
             Assert.IsInstanceOf<ICarsSystemDbContext>(db);
         }
     }
