bf4a0aeef9b5890e532e2d8e7ef4a28e5385af6d
todor-enikov <enigmaenikov@gmail.com>
2017-03-15 18:26:58 +0200

    Created Error Area.

---
+++ Source/Web/CarsSystem.WebClient.MVC/Areas/Error/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarsSystem.WebClient.MVC.Areas.Error.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error/Error
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Source/Web/CarsSystem.WebClient.MVC/Areas/Error/ErrorAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CarsSystem.WebClient.MVC.Areas.Error
+{
+    public class ErrorAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Error";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Error_default",
+                "Error/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
