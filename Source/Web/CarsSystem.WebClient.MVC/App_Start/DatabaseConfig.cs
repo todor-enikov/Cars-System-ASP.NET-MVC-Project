8958ff319a8513373d28be9fb974fb77d1a76d31
todor-enikov <enigmaenikov@gmail.com>
2017-03-15 15:42:54 +0200

    Added MVC client for the project and set it with the database. Database creating successfully.

---
+++ Source/Web/CarsSystem.WebClient.MVC/App_Start/DatabaseConfig.cs
@@ -0,0 +1,19 @@
+﻿using CarsSystem.Data;
+using CarsSystem.Data.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CarsSystem.WebClient.MVC.App_Start
+{
+    public class DatabaseConfig
+    {
+        public static void Initialize()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<CarsSystemDbContext, Configuration>());
+            CarsSystemDbContext.Create().Database.Initialize(true);
+        }
+    }
+}
---
+++ Source/Web/CarsSystem.WebClient.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CarsSystem.WebClient.MVC.MvcApplication" Language="C#" %>
