ff92ad2de9bd7a683fe1fc3f0fc6d589d2fed033
todor-enikov <enigmaenikov@gmail.com>
2017-03-16 00:06:53 +0200

    Refactored and readed unit tests for CarsSystemDbContext.

---
+++ Source/Tests/CarsSystem.Tests/Data/CarsSystem.Data/CarsSystemDbContextTests/CreateMethod.cs
@@ -0,0 +1,29 @@
+﻿using CarsSystem.Data;
+using NUnit.Framework;
+
+namespace CarsSystem.Tests.Data.CarsSystem.Data.CarsSystemDbContextTests
+{
+    [TestFixture]
+    public class CreateMethod
+    {
+        [Test]
+        public void ShouldReturnInstanceOfCarsDbContext()
+        {
+            // Arrange
+            var db = CarsSystemDbContext.Create();
+
+            // Act & Assert
+            Assert.IsInstanceOf<CarsSystemDbContext>(db);
+        }
+
+        [Test]
+        public void ShouldReturnInstanceOfICarsDbContext()
+        {
+            // Arrange
+            var db = CarsSystemDbContext.Create();
+
+            // Act & Assert
+            Assert.IsInstanceOf<ICarsSystemDbContext>(db);
+        }
+    }
+}
---
+++ Source/Tests/CarsSystem.Tests/Data/CarsSystem.Data/CarsSystemDbContextTests/Should.cs
@@ -0,0 +1,29 @@
+﻿using CarsSystem.Data;
+using NUnit.Framework;
+
+namespace CarsSystem.Tests.Data.CarsSystem.Data.CarsSystemDbContextTests
+{
+    [TestFixture]
+    public class Should
+    {
+        [Test]
+        public void CreateInstanceOfDatabase()
+        {
+            // Arrange
+            var db = new CarsSystemDbContext();
+
+            // Act & Assert
+            Assert.IsInstanceOf<CarsSystemDbContext>(db);
+        }
+
+        [Test]
+        public void BeTypeOfICarsDbContext()
+        {
+            // Arrange
+            var db = new CarsSystemDbContext();
+
+            // Act & Assert
+            Assert.IsInstanceOf<ICarsSystemDbContext>(db);
+        }
+    }
+}
