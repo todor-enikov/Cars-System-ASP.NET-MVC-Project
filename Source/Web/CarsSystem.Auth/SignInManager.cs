80bbfc8b5b6a7c92027296a0b61ea9028cee5304
todor-enikov <enigmaenikov@gmail.com>
2017-03-15 15:38:52 +0200

    Added CarsSystem.Auth dll needed for the project.

---
+++ Source/Web/CarsSystem.Auth/SignInManager.cs
@@ -0,0 +1,26 @@
+﻿using CarsSystem.Data.Models;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CarsSystem.Auth
+{
+    public class SignInManager : SignInManager<User, string>
+    {
+        public SignInManager(UserManager userManager, IAuthenticationManager authenticationManager) :
+            base(userManager, authenticationManager)
+        { }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(User user)
+        {
+            return user.GenerateUserIdentityAsync((UserManager)UserManager);
+        }
+
+        public static SignInManager Create(IdentityFactoryOptions<SignInManager> options, IOwinContext context)
+        {
+            return new SignInManager(context.GetUserManager<UserManager>(), context.Authentication);
+        }
+    }
+}
