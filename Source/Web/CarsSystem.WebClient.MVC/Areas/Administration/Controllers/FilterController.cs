3c97ab2209ec837f66e67ee317c6012e6ce901d0
todor-enikov <enigmaenikov@gmail.com>
2017-03-17 19:34:42 +0200

    Added FilterController and initial filter pages.

---
+++ Source/Web/CarsSystem.WebClient.MVC/Areas/Administration/Controllers/FilterController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarsSystem.WebClient.MVC.Areas.Administration.Controllers
+{
+    public class FilterController : Controller
+    {
+        // GET: Administration/Filter
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Administration/FilterByAnnualCheckUp
+        public ActionResult FilterByAnnualCheckUp()
+        {
+            return View();
+        }
+
+        // GET: Administration/FilterByVignette
+        public ActionResult FilterByVignette()
+        {
+            return View();
+        }
+
+        // GET: Administration/FilterByInsurance
+        public ActionResult FilterByInsurance()
+        {
+            return View();
+        }
+    }
+}
