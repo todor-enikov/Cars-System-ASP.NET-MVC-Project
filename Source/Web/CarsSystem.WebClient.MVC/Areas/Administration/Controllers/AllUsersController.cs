5771f3ea19faef5f767a2bd5848d9c25bd3db76d
todor-enikov <enigmaenikov@gmail.com>
2017-03-17 19:48:30 +0200

    Added initial pages for all users and all cars.

---
+++ Source/Web/CarsSystem.WebClient.MVC/Areas/Administration/Controllers/AllUsersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarsSystem.WebClient.MVC.Areas.Administration.Controllers
+{
+    public class AllUsersController : Controller
+    {
+        // GET: Administration/AllUsers
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
