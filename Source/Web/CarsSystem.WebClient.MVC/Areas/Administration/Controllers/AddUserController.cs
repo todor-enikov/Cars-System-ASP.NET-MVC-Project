cf0698364f299327a92f88eca749d744c9c37848
todor-enikov <enigmaenikov@gmail.com>
2017-03-17 19:05:49 +0200

    Created area Administration and initial view for adding user page.

---
+++ Source/Web/CarsSystem.WebClient.MVC/Areas/Administration/Controllers/AddUserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarsSystem.WebClient.MVC.Areas.Administration.Controllers
+{
+    public class AddUserController : Controller
+    {
+        // GET: Administration/AddUser
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+    }
+}
