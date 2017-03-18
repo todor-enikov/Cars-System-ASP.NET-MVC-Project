d7e88eefc6dd4ef720ed9a46dcfcf28239486204
todor-enikov <enigmaenikov@gmail.com>
2017-03-18 17:10:47 +0200

    Added bundles for jquery.

--- Source/Web/CarsSystem.WebClient.MVC/App_Start/BundleConfig.cs
+++ Source/Web/CarsSystem.WebClient.MVC/App_Start/BundleConfig.cs
@@ -26,6 +26,14 @@ namespace CarsSystem.WebClient.MVC
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            //Create bundel for jQueryUI  
+            //js  
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                      "~/Scripts/jquery-ui-{version}.js"));
+            //css  
+            bundles.Add(new StyleBundle("~/Content/cssjqryUi").Include(
+                   "~/Content/themes/base/jquery-ui.css"));
         }
     }
 }
