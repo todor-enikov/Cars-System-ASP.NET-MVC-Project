f0c71a275eae2c3efdf59823b23f6052f9e1c319
todor-enikov <enigmaenikov@gmail.com>
2017-03-19 20:17:22 +0200

    Implemented action result method for showing all cars and car by id.Created and views for them.

--- Source/Web/CarsSystem.WebClient.MVC/Areas/Administration/Controllers/CarsController.cs
+++ Source/Web/CarsSystem.WebClient.MVC/Areas/Administration/Controllers/CarsController.cs
@@ -1,4 +1,5 @@
 ﻿using CarsSystem.Services.Contracts;
+using CarsSystem.WebClient.MVC.Areas.Administration.Models.Cars;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,12 +18,50 @@ namespace CarsSystem.WebClient.MVC.Areas.Administration.Controllers
             this.service = service;
         }
 
-        // GET: Administration/Cars
+        [HttpGet]
         public ActionResult Index()
         {
             var carModel = this.service.GetAllCars().ToList();
+            var viewModel = new List<ShowAllCarsViewModel>();
 
-            return View(carModel);
+            foreach (var car in carModel)
+            {
+                var currentCar = new ShowAllCarsViewModel()
+                {
+                    Id = car.Id,
+                    Manufacturer = car.Manufacturer,
+                    Model = car.Model,
+                    RegistrationNumber = car.RegistrationNumber
+                };
+
+                viewModel.Add(currentCar);
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public ActionResult Details(Guid id)
+        {
+            var carModelById = this.service.GetCarById(id);
+            var viewModel = new CarDetailsViewModel()
+            {
+                Manufacturer = carModelById.Manufacturer,
+                Model = carModelById.Model,
+                TypeOfEngine = carModelById.TypeOfEngine,
+                RegistrationNumber = carModelById.RegistrationNumber,
+                VINNumber = carModelById.VINNumber,
+                CountOfTyres = carModelById.CountOfTyres,
+                CountOfDoors = carModelById.CountOfDoors,
+                TypeOfCar = carModelById.TypeOfCar,
+                YearOfManufactoring = carModelById.YearOfManufacturing,
+                ValidUntilAnnualCheckUp = carModelById.ValidUntilAnnualCheckUp,
+                ValidUntilVignette = carModelById.ValidUntilVignette,
+                ValidUntilInsurance = carModelById.ValidUntilVignette,
+                CarOwnerId = carModelById.UserId
+            };
+
+            return View(viewModel);
         }
     }
 }
