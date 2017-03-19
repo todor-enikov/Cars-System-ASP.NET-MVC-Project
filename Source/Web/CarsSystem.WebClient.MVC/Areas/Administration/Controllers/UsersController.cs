99eea42d0a074783df0acac7803bf40ec761b71d
todor-enikov <enigmaenikov@gmail.com>
2017-03-19 20:18:57 +0200

    Implemented action results for all users  and get usr by id. Created views and partial view _AllUsers for them.

--- Source/Web/CarsSystem.WebClient.MVC/Areas/Administration/Controllers/UsersController.cs
+++ Source/Web/CarsSystem.WebClient.MVC/Areas/Administration/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CarsSystem.Services.Contracts;
+using CarsSystem.WebClient.MVC.Areas.Administration.Models.Users;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,86 @@ namespace CarsSystem.WebClient.MVC.Areas.Administration.Controllers
 {
     public class UsersController : Controller
     {
-        // GET: Administration/AllUsers
+        private readonly IUsersService userService;
+        private readonly ICarsService carsService;
+
+        public UsersController(IUsersService userService, ICarsService carsService)
+        {
+            this.userService = userService;
+            this.carsService = carsService;
+        }
+
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var userModel = this.userService.GetAllUsers().ToList();
+            var viewModel = new List<ShowAllUsersViewModel>();
+
+            foreach (var user in userModel)
+            {
+                var currentUser = new ShowAllUsersViewModel()
+                {
+                    Id = user.Id,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Username = user.UserName
+                };
+
+                viewModel.Add(currentUser);
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public ActionResult Details(string id)
+        {
+            var userModelById = this.userService.GetUserById(id);
+            //var caridd = userModelById.Cars.FirstOrDefault().Id;
+
+            var viewModel = new UserDetailsViewModel()
+            {
+                Username = userModelById.UserName,
+                FirstName = userModelById.FirstName,
+                SecondName = userModelById.SecondName,
+                LastName = userModelById.LastName,
+                Egn = userModelById.EGN,
+                NumberOfIdCard = userModelById.NumberOfIdCard,
+                DateOfIssue = userModelById.DateOfIssue,
+                City = userModelById.City,
+                PhoneNumber = userModelById.PhoneNumber,
+                Email = userModelById.Email,
+                //CarId = this.carsService.GetCarId(userModelById)
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Search(string search)
+        {
+
+            if (string.IsNullOrEmpty(search))
+            {
+                return RedirectToAction("InternalServer", "Error", new { area = "Error" });
+            }
+
+            var userModel = this.userService.GetUserByEGN(long.Parse(search)).ToList();
+            var viewModel = new List<ShowAllUsersViewModel>();
+
+            foreach (var user in userModel)
+            {
+                var currentUser = new ShowAllUsersViewModel()
+                {
+                    Id = user.Id,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Username = user.UserName
+                };
+
+                viewModel.Add(currentUser);
+            }
+
+            return View("Index", viewModel);
         }
     }
 }
