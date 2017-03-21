94972f7fb711a2c390b4bcdf96c29b5651e7bc8f
todor-enikov <enigmaenikov@gmail.com>
2017-03-21 11:21:55 +0200

    Finished implementation for FilterByAnnualCheckUp view.

--- Source/Web/CarsSystem.WebClient.MVC/Areas/Administration/Controllers/FilterController.cs
+++ Source/Web/CarsSystem.WebClient.MVC/Areas/Administration/Controllers/FilterController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CarsSystem.Services.Contracts;
+using CarsSystem.WebClient.MVC.Areas.Administration.Models.Filter;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,25 +10,65 @@ namespace CarsSystem.WebClient.MVC.Areas.Administration.Controllers
 {
     public class FilterController : Controller
     {
-        // GET: Administration/Filter
+        private readonly IFilterService filterService;
+        private readonly IMailService mailService;
+
+        public FilterController(IFilterService filterService, IMailService mailService)
+        {
+            this.filterService = filterService;
+            this.mailService = mailService;
+        }
+
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
-        // GET: Administration/FilterByAnnualCheckUp
+        [HttpGet]
         public ActionResult FilterByAnnualCheckUp()
         {
-            return View();
+            var filterModel = this.filterService
+                                  .FilterExpiringAnnualCheckUp()
+                                  .ToList();
+            var viewModel = new List<FilterViewModel>();
+
+            foreach (var filter in filterModel)
+            {
+                var currentFilter = new FilterViewModel()
+                {
+                    Manufacturer = filter.Manufacturer,
+                    Model = filter.Model,
+                    RegistrationNumber = filter.RegistrationNumber,
+                    VINNumber = filter.VINNumber,
+                    ExpirationDate = filter.ValidUntilAnnualCheckUp
+                };
+
+                viewModel.Add(currentFilter);
+            }
+
+            return View(viewModel);
         }
 
-        // GET: Administration/FilterByVignette
+        [HttpPost]
+        public ActionResult FilterByAnnualCheckUp(string emailSubjectTextBox, string emailContentBox)
+        {
+            var emails = this.filterService
+                             .GetMailsForCarsAnnualCheckUpExpiration()
+                             .ToList();
+
+            this.mailService.SendEmail(emailSubjectTextBox, emailContentBox, emails);
+
+            return RedirectToAction("Index", "Home", new { area = "" });
+        }
+
+        [HttpGet]
         public ActionResult FilterByVignette()
         {
             return View();
         }
 
-        // GET: Administration/FilterByInsurance
+        [HttpGet]
         public ActionResult FilterByInsurance()
         {
             return View();
