3cba426dea66640aee063d4919e03a06e36cf094
todor-enikov <enigmaenikov@gmail.com>
2017-03-21 11:34:07 +0200

    Finished implementation for FilterByVignette and FilterByInsurance views.

--- Source/Web/CarsSystem.WebClient.MVC/Areas/Administration/Controllers/FilterController.cs
+++ Source/Web/CarsSystem.WebClient.MVC/Areas/Administration/Controllers/FilterController.cs
@@ -65,13 +65,75 @@ namespace CarsSystem.WebClient.MVC.Areas.Administration.Controllers
         [HttpGet]
         public ActionResult FilterByVignette()
         {
-            return View();
+            var filterModel = this.filterService
+                                  .FilterExpiringVignetteCars()
+                                  .ToList();
+            var viewModel = new List<FilterViewModel>();
+
+            foreach (var filter in filterModel)
+            {
+                var currentFilter = new FilterViewModel()
+                {
+                    Manufacturer = filter.Manufacturer,
+                    Model = filter.Model,
+                    RegistrationNumber = filter.RegistrationNumber,
+                    VINNumber = filter.VINNumber,
+                    ExpirationDate = filter.ValidUntilAnnualCheckUp
+                };
+
+                viewModel.Add(currentFilter);
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult FilterByVignette(string emailSubjectTextBox, string emailContentBox)
+        {
+            var emails = this.filterService
+                             .GetMailsForCarsVignetteExpiration()
+                             .ToList();
+
+            this.mailService.SendEmail(emailSubjectTextBox, emailContentBox, emails);
+
+            return RedirectToAction("Index", "Home", new { area = "" });
         }
 
         [HttpGet]
         public ActionResult FilterByInsurance()
         {
-            return View();
+            var filterModel = this.filterService
+                                  .FilterExpiringInsurance()
+                                  .ToList();
+            var viewModel = new List<FilterViewModel>();
+
+            foreach (var filter in filterModel)
+            {
+                var currentFilter = new FilterViewModel()
+                {
+                    Manufacturer = filter.Manufacturer,
+                    Model = filter.Model,
+                    RegistrationNumber = filter.RegistrationNumber,
+                    VINNumber = filter.VINNumber,
+                    ExpirationDate = filter.ValidUntilAnnualCheckUp
+                };
+
+                viewModel.Add(currentFilter);
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult FilterByInsurance(string emailSubjectTextBox, string emailContentBox)
+        {
+            var emails = this.filterService
+                             .GetMailsForCarsInsuranceExpiration()
+                             .ToList();
+
+            this.mailService.SendEmail(emailSubjectTextBox, emailContentBox, emails);
+
+            return RedirectToAction("Index", "Home", new { area = "" });
         }
     }
 }
