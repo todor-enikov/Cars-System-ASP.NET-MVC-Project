dd36368d1cbb822e9e82e166a9d22a56f610bda4
todor-enikov <enigmaenikov@gmail.com>
2017-03-21 11:37:49 +0200

    Removed async Task from ManageController.

--- Source/Web/CarsSystem.WebClient.MVC/Controllers/ManageController.cs
+++ Source/Web/CarsSystem.WebClient.MVC/Controllers/ManageController.cs
@@ -53,7 +53,7 @@ namespace CarsSystem.WebClient.MVC.Controllers
 
         //
         // GET: /Manage/Index
-        public async Task<ActionResult> Index(ManageMessageId? message)
+        public ActionResult Index(ManageMessageId? message)
         {
             ViewBag.StatusMessage =
                 message == ManageMessageId.ChangePasswordSuccess ? "Your password has been changed."
