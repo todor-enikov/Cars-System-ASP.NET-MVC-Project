f77aebb467142a268dd71205343eb542cc7a6956
todor-enikov <enigmaenikov@gmail.com>
2017-03-21 11:20:50 +0200

    Created model FilterViewModel.

---
+++ Source/Web/CarsSystem.WebClient.MVC/Areas/Administration/Models/Filter/FilterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarsSystem.WebClient.MVC.Areas.Administration.Models.Filter
+{
+    public class FilterViewModel
+    {
+        public string Manufacturer { get; set; }
+
+        public string Model { get; set; }
+
+        public string RegistrationNumber { get; set; }
+
+        public string VINNumber { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+    }
+}
