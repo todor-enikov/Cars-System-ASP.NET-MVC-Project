7d3ac78eb78083f4462035319b3b6e1b7ced9931
todor-enikov <enigmaenikov@gmail.com>
2017-03-23 13:36:45 +0200

    Implemented logic for updating user information on edit user page.

--- Source/Web/CarsSystem.WebClient.MVC/Areas/Administration/Controllers/UsersController.cs
+++ Source/Web/CarsSystem.WebClient.MVC/Areas/Administration/Controllers/UsersController.cs
@@ -1,6 +1,11 @@
-﻿using CarsSystem.Services.Contracts;
+﻿using CarsSystem.Auth;
+using CarsSystem.Data.Models;
+using CarsSystem.Services.Contracts;
+using CarsSystem.WebClient.MVC.Areas.Administration.Models;
 using CarsSystem.WebClient.MVC.Areas.Administration.Models.Users;
 using Common;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,11 +19,29 @@ namespace CarsSystem.WebClient.MVC.Areas.Administration.Controllers
     {
         private readonly IUsersService userService;
         private readonly ICarsService carsService;
+        private UserManager _userManager;
 
         public UsersController(IUsersService userService, ICarsService carsService)
         {
             this.userService = userService;
             this.carsService = carsService;
+        }
+
+        public UsersController(UserManager userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public UserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<UserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
         }
 
         [HttpGet]
@@ -51,6 +74,7 @@ namespace CarsSystem.WebClient.MVC.Areas.Administration.Controllers
 
             var viewModel = new UserDetailsViewModel()
             {
+                Id = userModelById.Id,
                 Username = userModelById.UserName,
                 FirstName = userModelById.FirstName,
                 SecondName = userModelById.SecondName,
@@ -95,6 +119,52 @@ namespace CarsSystem.WebClient.MVC.Areas.Administration.Controllers
             }
 
             return View("Index", viewModel);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            var userModelById = this.userService.GetUserById(id);
+            var viewModel = new UserViewModel()
+            {
+                Id = userModelById.Id,
+                Username = userModelById.UserName,
+                FirstName = userModelById.FirstName,
+                SecondName = userModelById.SecondName,
+                LastName = userModelById.LastName,
+                Egn = userModelById.EGN,
+                NumberOfIdCard = userModelById.NumberOfIdCard,
+                DateOfIssue = userModelById.DateOfIssue,
+                City = userModelById.City,
+                PhoneNumber = userModelById.PhoneNumber,
+                Email = userModelById.Email
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(UserViewModel user)
+        {
+            var userToUpdate = new User()
+            {
+                Id = user.Id,
+                UserName = user.Username,
+                FirstName = user.FirstName,
+                SecondName = user.SecondName,
+                LastName = user.LastName,
+                EGN = user.Egn,
+                NumberOfIdCard = user.NumberOfIdCard,
+                DateOfIssue = user.DateOfIssue,
+                City = user.City,
+                PhoneNumber = user.PhoneNumber,
+                Email = user.Email,
+            };
+
+            this.userService.Update(userToUpdate);
+            UserManager.AddPassword(userToUpdate.Id, "123456");
+
+            return RedirectToAction("Details", "Users", new { area = "Administration", id = user.Id });
         }
     }
 }
