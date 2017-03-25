f21a100e6dd73b72622d111a32f9bbe2fb6fc93b
todor-enikov <enigmaenikov@gmail.com>
2017-03-25 17:24:33 +0200

    Added constants in common library.

--- Source/Common/Common/ApplicationConstants.cs
+++ Source/Common/Common/ApplicationConstants.cs
@@ -2,7 +2,14 @@
 {
     public class ApplicationConstants
     {
+        // User Roles
         public const string AdminRole = "Admin";
         public const string UserRole = "User";
+
+        // Exception Messages
+        public const string UsersServiceErrorMessage = "Users service should not be null";
+        public const string CarssServiceErrorMessage = "Cars service should not be null";
+        public const string FilterServiceErrorMessage = "Filter service should not be null";
+        public const string MailServiceErrorMessage = "Mail service should not be null";
     }
 }
