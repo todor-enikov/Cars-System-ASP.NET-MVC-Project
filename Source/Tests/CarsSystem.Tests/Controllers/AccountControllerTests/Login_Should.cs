d41541ef76bd05900038bdff85f2441f53338e1c
todor-enikov <enigmaenikov@gmail.com>
2017-03-25 17:11:23 +0200

    Added Account controllers unit tests.

---
+++ Source/Tests/CarsSystem.Tests/Controllers/AccountControllerTests/Login_Should.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using CarsSystem.WebClient.MVC.Controllers;
+using TestStack.FluentMVCTesting;
+
+namespace CarsSystem.Tests.Controllers.AccountControllerTests
+{
+    [TestFixture]
+    public class Login_Should
+    {
+        [Test]
+        public void ReturnViewWithReturnUrlInViewBag()
+        {
+            // Arrange
+            var accountController = new AccountController();
+            string returnUrl = "url";
+
+            // Act
+            accountController
+                            .WithCallTo(c => c.Login(returnUrl))
+                            .ShouldRenderDefaultView();
+
+            // Assert
+            Assert.AreEqual(returnUrl, accountController.ViewBag.ReturnUrl);
+        }
+    }
+}
