c2ee15440ebef5cd3a71b2bb169b6f86c4d8760a
todor-enikov <enigmaenikov@gmail.com>
2017-03-25 17:11:51 +0200

    Added Manage controller unit tests.

---
+++ Source/Tests/CarsSystem.Tests/Controllers/ManageControllerTests/ChangePassword_Should.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+using CarsSystem.WebClient.MVC.Controllers;
+using TestStack.FluentMVCTesting;
+using CarsSystem.WebClient.MVC.Models;
+
+namespace CarsSystem.Tests.Controllers.ManageControllerTests
+{
+    [TestFixture]
+    public class ChangePassword_Should
+    {
+        [Test]
+        public void ReturnDefaultView()
+        {
+            // Arrange
+            var manageController = new ManageController();
+
+            // Act & Assert
+            manageController
+                            .WithCallTo(c => c.ChangePassword())
+                            .ShouldRenderDefaultView();
+        }
+
+        [Test]
+        public void ReturnExpectedViewModel()
+        {
+            // Arrange
+            var model = new ChangePasswordViewModel();
+            var manageController = new ManageController();
+
+            // Act & Assert
+            manageController
+                            .WithModelErrors()
+                            .WithCallTo(c => c.ChangePassword(model))
+                            .ShouldRenderDefaultView()
+                            .WithModel(model);
+        }
+    }
+}
