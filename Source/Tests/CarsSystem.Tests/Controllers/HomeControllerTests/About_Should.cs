ebbb8a041f8c16bac90efe147f918333ded88fa7
todor-enikov <enigmaenikov@gmail.com>
2017-03-25 17:12:16 +0200

    Added Home controller unit tests.

---
+++ Source/Tests/CarsSystem.Tests/Controllers/HomeControllerTests/About_Should.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using CarsSystem.WebClient.MVC.Controllers;
+using TestStack.FluentMVCTesting;
+
+namespace CarsSystem.Tests.Controllers.HomeControllerTests
+{
+    [TestFixture]
+    public class About_Should
+    {
+        [Test]
+        public void ReturnDefaulView()
+        {
+            // Arrange
+            var homeController = new HomeController();
+
+            // Act & Assert
+            homeController
+                          .WithCallTo(c => c.About())
+                          .ShouldRenderDefaultView();
+        }
+    }
+}
---
+++ Source/Tests/CarsSystem.Tests/Controllers/HomeControllerTests/Contact_Should.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using CarsSystem.WebClient.MVC.Controllers;
+using TestStack.FluentMVCTesting;
+
+namespace CarsSystem.Tests.Controllers.HomeControllerTests
+{
+    [TestFixture]
+    public class Contact_Should
+    {
+        [Test]
+        public void ReturnDefaultView()
+        {
+            // Arrange
+            var homeController = new HomeController();
+
+            // Act & Assert
+            homeController
+                           .WithCallTo(c => c.Contact())
+                           .ShouldRenderDefaultView();
+        }
+    }
+}
